2835f10c5ffe22d9205874cc185e111ec5ad269b
Rita de Cássia Leite Nascimento <sarahsetej@gmail.com>
2022-02-28 15:33:36 -0300

    Ribbon load balancing

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/feignclients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/feignclients/WorkerFeignClient.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.devsuperior.hrpayroll.entities.Worker;
 
 @Component
-@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+@FeignClient(name = "hr-worker", path = "/workers")//com mais de uma stância rodando
+//@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")//=> com só uma stância rodando
 public interface WorkerFeignClient {
 	
 	@GetMapping(value = "/{id}")
